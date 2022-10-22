e214c135ff2cd77431bdf76c67f72c48241b14ae
oumaymaagherai <agherai06@gmail.com>
2022-10-22 19:04:29 +0100

    Eureka server

---
+++ eureka-discovery-service/src/test/java/org/enset/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.enset.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
